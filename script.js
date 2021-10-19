ff97f746275f664faaa820ff51ed7f759b6f0dcc
zhumad1lb <92530126+zhumad1lb@users.noreply.github.com>
2021-10-19 12:17:02 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function myparis() {
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $(".myparis").offset().top
+    }, 1000);
+}
+
+function member() {
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $(".member").offset().top
+    }, 1000);
+}
+
+function forma() {
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $(".forma").offset().top
+    }, 1000);
+}
+function slider() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".slider").offset().top
+    }, 1000);
+}
+
+function bilet() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".bilet").offset().top
+    }, 1000);
+}
